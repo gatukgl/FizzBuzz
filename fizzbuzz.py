019ed834890bda38af578658ff98fb4adfa3010f
Gatukgl S. Chattanon <gatukgl@prontolap0009.saiyajin.com>
2014-07-31 11:35:30 +0700

    write unittest for test case number 3 should return fizz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,17 @@
-yeah = ''
+import unittest
+
+
+class FizzBuzzTest(unittest.TestCase):
+    def test_number_3_should_return_3(self):
+        fizzbuzz = FizzBuzz()
+        result = fizzbuzz.take(3)
+        self.assertEqual(result, 'fizz')
+
+
+class FizzBuzz:
+    def take(self, number):
+        return 'fizz'
+
+
+if __name__ == "__main__":
+    unittest.main()
